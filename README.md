36be4c41480162bbfc238ae78a2c35e0f0356876
Kirubakaran, Anitha <anitha.kirubakaran@wirecard.com>
2020-08-06 11:15:26 +0530

    Included README.md and reformatted space

---
+++ README.md
@@ -0,0 +1,8 @@
+# DynamicDB-Task
+
+Single API Call to retrieve values from the database by dynamically providing table name as a parameter.
+
+## Versioning
+
+* **`1.0.0`**
+    - Initial release of an Application
--- src/main/java/com/assissment/m2p/service/DynamicTableServiceImpl.java
+++ src/main/java/com/assissment/m2p/service/DynamicTableServiceImpl.java
@@ -4,6 +4,7 @@ import com.assissment.m2p.exception.TableNameNotExist;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Map;
 
@@ -13,7 +14,7 @@ public class DynamicTableServiceImpl {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    public List<Map<String, Object>> fetchAllDetails(String tableName){
+    public List<Map<String, Object>> fetchAllDetails(String tableName) {
         StringBuilder br = new StringBuilder("Select * from ").append(tableName);
         return jdbcTemplate.queryForList(br.toString());
     }
